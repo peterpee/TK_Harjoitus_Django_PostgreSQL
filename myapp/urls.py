3d25cef7f85fc97cef2e4c0e55f78e5fcc75a4eb
peterpee <31952782+peterpee@users.noreply.github.com>
2023-10-14 14:44:06 +0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,7 @@
 from . import views
-
 from django.urls import path
 from .views import SignUpView
 from .views import logout_view
-
-
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import logout
 # from mysite import settings
@@ -15,7 +12,6 @@ urlpatterns = [
     path('create/', views.create_recipe, name='create_recipe'),
     path('view/<int:recipe_id>/', views.view_recipe, name='view_recipe'),
     path('logout/', views.logout_view, name='logout_user'),
-    # path('delete_recipe/(?P<pk>.*)', views.delete_recipe, name='delete'),
     path('delete_recipe/<int:recipe_id>/', views.delete_recipe, name='delete'),
 ]
 
